67f40e5eb4e4f1c39142799ef1e2836f67ab306b
Adrian <zooyoowoo@gmail.com>
2018-09-12 11:53:09 +0800

    implicit

--- src/main/scala/unit15_implicit/ImplicitDemo.scala
+++ src/main/scala/unit15_implicit/ImplicitDemo.scala
@@ -1,7 +1,7 @@
 package unit15_implicit
 
 import java.io.File
-import java.util.Date
+import java.util.{Calendar, Date}
 
 import scala.io.Source
 
@@ -83,7 +83,13 @@ object ImplicitDemo1 {
     var date = new Date()
     implicit class date2RichDate(val date: Date) { //隐式类
       //def +(i: Int) = new Date(date.getTime+i*1000)//加一秒
-      def +(i: Int) = new Date(date.getTime + i * 1000 * 3600 * 24)
+//      def +(i: Int) = new Date(date.getTime + i * 1000 * 3600 * 24)//加一天
+      def +(i: Int)= {
+        val calendar = Calendar.getInstance()
+        calendar.setTime(date)
+        calendar.add(Calendar.DAY_OF_YEAR, i)
+        calendar.getTime
+      }   //标准的加一天
     }
 
     date = date + 1 // 默认是报错的，请通过隐式转换实现为：给当前日期加1秒的效果
